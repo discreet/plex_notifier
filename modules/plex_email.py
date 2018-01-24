924c1d59229b57350c8416e64f02d72f475e277e
discr33t <cpisano86@gmail.com>
2018-01-24 18:40:45 -0500

    Email Message Body
    This addresses Issue #2. The original email message body was not well
    formatted being that the list of media was an array. The new message
    body has the media formatted as a string with newline charaters as the
    delimiter.
    
    Signed-off-by: discr33t <cpisano86@gmail.com>

--- modules/plex_email.py
+++ modules/plex_email.py
@@ -4,20 +4,27 @@ from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 import smtplib
 
+def format_media(media):
+    return '\n'.join(media)
+
 def send_mail(username, password, email_list, media):
+    media = format_media(media)
+    unsub = 'To Unsubscribe from this list please reply with the word "Unsubscribe".'
+    email_body = 'Recently Added to Plex\n\n%s\n\n%s' % (media, unsub)
+
     for email in email_list:
         msg = MIMEMultipart()
         msg['From'] = username
         msg['To'] = email
         msg['Subject'] = 'Plex Update: New Media Added'
 
-        body = "Recently Added to Plex\n"
+        body = email_body
         msg.attach(MIMEText(body, 'plain'))
 
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
         server.login(username, password)
-        text = ms.as_string()
+        text = msg.as_string()
         server.sendmail(username, email, text)
         server.quit()
 
