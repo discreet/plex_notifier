24f8c3d99339c6027fe075553f21d03861515ddc
Chris Pisano <cpisano86@gmail.com>
2018-02-09 20:08:39 -0500

    Implemented Jinja Template and Python 3 (#29)
    Switched to Python 3, Pip3 utilizing pipenv to manage dependencies.
    Updated Tusk and Travis accordingly. Modified email to use a Jinja
    template to format the body and changed to a better email library.
    
    Signed-off-by: discr33t <cpisano86@gmail.com>

--- plex_notifier/plex_email.py
+++ plex_notifier/plex_email.py
@@ -1,46 +1,40 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 This module formats the returned plex media and builds a proper email message.
 It will then send an email to all of the subscribed plex users.
 """
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-import smtplib
 import re
+import yagmail
+import jinja2
 
-def __format_media(media):
+def __format_body(media):
     """
-    Takes the array of recently added media and converts it to a string using a
-    newline character as a delimeter.
+    foo
     """
-    return '\n'.join(media)
+    loader = jinja2.FileSystemLoader(searchpath='templates')
+    template_env = jinja2.Environment(loader=loader)
+    template = template_env.get_template('email')
 
-def send_mail(username, password, email_list, plex_server, media):
+    return template.render(media=media)
+
+def send_mail(email_attrs, plex_server, new_media):
     """
     Takes the email username and password, list of email recipients, plex server
     name and formatted media to compose and send the email to the subscribed
     users.
     """
-    media = __format_media(media)
-    unsub = 'To Unsubscribe from this list please reply with the word "Unsubscribe".'
-    email_body = 'Recently Added to Plex\n\n%s\n\n%s' % (media, unsub)
+    yag = yagmail.SMTP(email_attrs.username, email_attrs.password)
+    body = __format_body(new_media)
+    subject = 'Plex Update: New Media Added to %s' % (plex_server)
     email_regex = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
 
-    for email in email_list:
-        msg = MIMEMultipart()
-        msg['From'] = username
-        msg['To'] = email
-        msg['Subject'] = 'Plex Update: New Media Added to %s' % (plex_server)
-
-        body = email_body
-        msg.attach(MIMEText(body, 'plain'))
-
-        if re.match(email_regex, email):
-            server = smtplib.SMTP('smtp.gmail.com', 587)
-            server.starttls()
-            server.login(username, password)
-            text = msg.as_string()
-            server.sendmail(username, email, text)
-            server.quit()
-
-            print 'Email sent to %s' % (email)
+    for obj in email_attrs.mailing_list:
+        if re.match(email_regex, obj):
+            yag.send(to=obj, subject=subject, contents=body)
+            print('Email sent to %s' % (obj))
+        else:
+            recipient = email_attrs.username
+            subject = 'Invalid Email Address'
+            contents = '%s is NOT a valid email address' % (obj)
+            print(contents)
+            yag.send(to=recipient, subject=subject, contents=contents)
